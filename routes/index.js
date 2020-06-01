102f492a279b163d92c767871d0ca8f53c0fa4a9
Michael Sanderson <jawm17@gmail.com>
2020-06-01 04:38:14 -0500

    p

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ const userRouter = require("./User.js");
 // API Routes
 router.use("/User", userRouter);
 
-// // If no API routes are hit, send the React app
-// router.use(function(req, res) {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = router;
