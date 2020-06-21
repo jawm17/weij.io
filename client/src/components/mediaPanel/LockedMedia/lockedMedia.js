aeb870cc314d26ad04dc36b792eac3d780e6dc3f
Michael Sanderson <jawm17@gmail.com>
2020-06-20 19:05:48 -0500

    initialized lockedMedia container

---
+++ client/src/components/mediaPanel/LockedMedia/lockedMedia.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState, useContext } from 'react';
+import { AuthContext } from '../../../context/AuthContext';
+
+export default function LockedMedia(props) {
+
+    return (
+        <div></div>
+    );
+}
