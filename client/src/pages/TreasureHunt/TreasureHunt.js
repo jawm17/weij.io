f74bc3d4a885af92e1e902bcf1e781c0fabe5126
Michael Sanderson <jawm17@gmail.com>
2020-07-09 10:59:08 -0700

    Mouse movement manipulates logo background

--- client/src/pages/TreasureHunt/TreasureHunt.js
+++ client/src/pages/TreasureHunt/TreasureHunt.js
@@ -2,20 +2,30 @@ import React, { useEffect, useState, useContext } from "react";
 import "./TreasureHunt.css";
 
 export default function TreasureHunt() {
+    const [mousePosition, setMousePosition] = useState({ x: null, y: null });
 
-const style = {
-    treasureHuntLogo : {
-        background: 'url("https://thumbs.dreamstime.com/b/tv-static-4941028.jpg")',
-        backgroundPositionX: 80,
-        fontFamily: "'Sonsie One', cursive",
-        position: "absolute",
-        bottom: 30,
-        left: 30,
-        "-webkit-text-fill-color": "transparent",
-        "-webkit-background-clip": "text"
+    const style = {
+        treasureHuntLogo: {
+            background: 'url("https://previews.123rf.com/images/stevanovicigor/stevanovicigor1508/stevanovicigor150800150/43924569-tv-damage-bad-sync-tv-channel-rgb-lcd-television-screen-with-static-noise-from-poor-broadcast-signal.jpg")',
+            backgroundPositionX: mousePosition.x,
+            backgroundPositionY: mousePosition.y,
+            fontFamily: "'Sonsie One', cursive",
+            position: "absolute",
+            bottom: 30,
+            left: 30,
+            WebkitTextFillColor: "transparent",
+            WebkitBackgroundClip: "text"
+        }
     }
-    
-}
+
+    useEffect(() => {
+        window.addEventListener("mousemove", mouseMove);
+        return () => window.removeEventListener("mousemove", mouseMove);
+    }, []);
+
+    function mouseMove(e) {
+        setMousePosition({ x: e.clientX, y: e.clientY });
+    }
 
     return (
         <div>
