d33b32a649c29341a7c48d84c8ae543d5df89f3b
Michael Sanderson <jawm17@gmail.com>
2020-08-27 18:40:15 -0700

    Test

---
+++ client/build/precache-manifest.5224889f48bd85f72d6e7eec1ed7f4be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac309c55bb66cb2e9d5aa0d77b7c2b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a90ee6ed408ec4acd02",
+    "url": "/static/css/main.8f7fc593.chunk.css"
+  },
+  {
+    "revision": "643b163a06f94ec23deb",
+    "url": "/static/js/2.39cf3b77.chunk.js"
+  },
+  {
+    "revision": "71f08bb8ad75542702a8ef6c3e6386cd",
+    "url": "/static/js/2.39cf3b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a90ee6ed408ec4acd02",
+    "url": "/static/js/main.fe23c129.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
