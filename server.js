8057f0ec64c3c8de6c0258480ce3e8fea2ca63c5
Michael Sanderson <jawm17@gmail.com>
2020-08-27 18:30:51 -0700

    Test

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  }); 
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   }); 
+// }
 // Add routes, both API and view
 app.use(router);
 
