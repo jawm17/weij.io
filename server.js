5ab3340990017bb84246d2e275fa245e1abbf442
Michael Sanderson <jawm17@gmail.com>
2020-08-27 18:08:43 -0700

    Missing dependency

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const router = require("./routes/index");
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -15,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  }); 
 }
 // Add routes, both API and view
 app.use(router);
