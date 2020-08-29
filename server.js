84c95800460b633d3dc76940e54ff9e435c9f45d
Michael Sanderson <jawm17@gmail.com>
2020-08-29 15:21:35 -0700

    Production routes

--- server.js
+++ server.js
@@ -11,13 +11,16 @@ const config = require('./config/config');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
+  // Set static folder
   app.use(express.static("client/build"));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   }); 
 }
+
 // Add routes, both API and view
 app.use(router);
 
