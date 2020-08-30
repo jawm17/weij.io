6458b920c1f282e7ee2778fab34a3f8fc8c52efd
Michael Sanderson <jawm17@gmail.com>
2020-08-29 21:37:29 -0700

    Inert seqeuntial data pragmatism

--- server.js
+++ server.js
@@ -16,9 +16,6 @@ app.use(cookieParser());
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
 }
 
 // Add routes, both API and view
