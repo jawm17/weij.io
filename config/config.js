71aee67e7b7fc48150d71a460fba81ee9336fae1
Michael Sanderson <jawm17@gmail.com>
2020-09-18 19:17:28 -0700

    Clean up ignored files

--- config/config.js
+++
@@ -1,9 +0,0 @@
-  
-// Copy this file as config.js in the same folder, with the proper database connection URI.
-
-module.exports = {
-    db: "mongodb+srv://koikoi5:koikoi5@mymodata-4ywri.mongodb.net/MyMoData?retryWrites=true&w=majority",
-    db_dev: "mongodb+srv://koikoi5:koikoi5@mymodata-4ywri.mongodb.net/MyMoData?retryWrites=true&w=majority",
-    AWS_A_KEYID: "AKIAITXRLE2MXN",
-    AWS_S_KEYID: "+Lxff9y+/LIPGanTRx1wL1u3mcD83X"
-};
