6c19fa614d8edfc4f7812c3b5ae5cb5ab6e70bdd
Michael Sanderson <jawm17@gmail.com>
2020-10-18 23:13:51 -0700

    Styling

--- client/src/pages/NewDemo/NewDemo.js
+++ client/src/pages/NewDemo/NewDemo.js
@@ -2,13 +2,22 @@ import React, { useEffect, useState } from 'react';
 import "./NewDemoStyle.css";
 
 export default function NewDemo() {
-    
+
     return (
         <div>
             <div id="header">
                 <h3 className="mainLogo">
                     ethereal
                 </h3>
+                <div className="buttonGroup">
+                    <div id="wallet">
+                    </div>
+                    <div id="profile">
+                    </div>
+                    <div id="message">
+                    </div>
+                </div>
+
             </div>
             <div className="outer">
                 <div className="row">
@@ -17,88 +26,88 @@ export default function NewDemo() {
 
                     </div>
                     <div className="item">
-                        
+
                     </div>
-                    <div className="item">
-                        
-                    </div>
-                    <div className="item">
-                        
-                    </div>
-                    <div className="item">
-                        
-                    </div>
-                    <div className="item">
-                        
-                    </div>
-                    <div className="item">
-                        
-                    </div>
-                    <div className="item">
-                        
-                    </div>
-                    <div className="item">
-                        
-                    </div>
-                </div>
-                <div className="row">
-                <h2 className="rowTitle">B00By736's Videos</h2>
                     <div className="item">
 
                     </div>
                     <div className="item">
-                        
+
                     </div>
-                    <div className="item">
-                        
-                    </div>
-                    <div className="item">
-                        
-                    </div>
-                    <div className="item">
-                        
-                    </div>
-                    <div className="item">
-                        
-                    </div>
-                    <div className="item">
-                        
-                    </div>
-                    <div className="item">
-                        
-                    </div>
-                    <div className="item">
-                        
-                    </div>
-                </div>
-                <div className="row">
-                <h2 className="rowTitle">For You</h2>
                     <div className="item">
 
                     </div>
                     <div className="item">
-                        
+
                     </div>
                     <div className="item">
-                        
+
                     </div>
                     <div className="item">
-                        
+
                     </div>
                     <div className="item">
-                        
+
+                    </div>
+                </div>
+                <div className="row">
+                    <h2 className="rowTitle">B00By736's Videos</h2>
+                    <div className="item">
+
                     </div>
                     <div className="item">
-                        
+
                     </div>
                     <div className="item">
-                        
+
                     </div>
                     <div className="item">
-                        
+
                     </div>
                     <div className="item">
-                        
+
+                    </div>
+                    <div className="item">
+
+                    </div>
+                    <div className="item">
+
+                    </div>
+                    <div className="item">
+
+                    </div>
+                    <div className="item">
+
+                    </div>
+                </div>
+                <div className="row">
+                    <h2 className="rowTitle">For You</h2>
+                    <div className="item">
+
+                    </div>
+                    <div className="item">
+
+                    </div>
+                    <div className="item">
+
+                    </div>
+                    <div className="item">
+
+                    </div>
+                    <div className="item">
+
+                    </div>
+                    <div className="item">
+
+                    </div>
+                    <div className="item">
+
+                    </div>
+                    <div className="item">
+
+                    </div>
+                    <div className="item">
+
                     </div>
                 </div>
             </div>
