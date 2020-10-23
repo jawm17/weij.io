ea6190323cd3721400fb4576048c3f9ab4925503
Michael Sanderson <jawm17@gmail.com>
2020-10-22 23:25:42 -0700

    Component org

--- client/src/components2/media.js
+++ client/src/components2/media.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from 'react';
 
-export default function Media() {
+export default function Media(props) {
+
+    // useEffect(() => {
+    //     openMedia
+    // }, []);
+
+
+    function openMedia(e) {
+        console.log(e);
+        let media = document.createElement("div");
+        let x = document.getElementById(e.target.id).getBoundingClientRect().x;
+        let y = document.getElementById(e.target.id).getBoundingClientRect().y;
+        media.setAttribute("class", "item");
+        media.style.margin = 0;
+        media.style.position = "fixed";
+        media.style.top = y + "px";
+        media.style.left = x + "px";
+        document.body.appendChild(media);
+        setTimeout(() => {
+            media.style.borderRadius = "0px";
+        }, 100)
+        setTimeout(() => {
+            media.style.width = "100vw";
+            media.style.height = "100vh";
+            media.style.top = "0px";
+            media.style.left = "0px";
+        }, 200)
+    }
 
     return (
-        <div>
-            hellow
+        <div className="item" id={props.id}  onClick={(e) => openMedia(e)}>
+            
         </div>
     );
 }
--- client/src/pages/NewDemo/NewDemo.js
+++ client/src/pages/NewDemo/NewDemo.js
@@ -1,28 +1,32 @@
-import { MediaStoreData } from 'aws-sdk';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import Media from "../../components2/media";
+import UserService from '../../services/UserService';
+import { AuthContext } from '../../context/AuthContext';
 import "./NewDemoStyle.css";
 
 export default function NewDemo() {
+    const [posts, setPosts] = useState([]);
+    const authContext = useContext(AuthContext);
 
-    function openMedia(e) {
-        let media = document.createElement("div");
-        let x = document.getElementById(e.target.id).getBoundingClientRect().x;
-        let y = document.getElementById(e.target.id).getBoundingClientRect().y;
-        media.setAttribute("class", "item");
-        media.style.margin = 0;
-        media.style.position = "fixed";
-        media.style.top = y + "px";
-        media.style.left = x + "px";
-        document.body.appendChild(media);
-        setTimeout(() => {
-            media.style.borderRadius = "0px";
-        }, 100)
-        setTimeout(() => {
-            media.style.width = "100vw";
-            media.style.height = "100vh";
-            media.style.top = "0px";
-            media.style.left = "0px";
-        }, 200)
+    useEffect(() => {
+        getFeed();
+    }, []);
+
+    function getFeed() {
+        UserService.getFeed().then(data => {
+            if (!data.message) {
+                console.log(data);
+                data.sort(function (a, b) {
+                    return new Date(b.createdAt) - new Date(a.createdAt);
+                });
+                setPosts(data);
+            }
+            else if (data.message.msgBody === "Unauthorized") {
+                authContext.setUser({ username: "" });
+                authContext.setIsAuthenticated(false);
+                window.alert("not logged in");
+            }
+        });
     }
 
     return (
@@ -33,11 +37,11 @@ export default function NewDemo() {
                 </h3>
                 <div className="buttonGroup">
                     <div id="wallet">
-                        
+
                         {/* <img id="ethLogo" src="https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Ethereum-ETH-icon.png" alt="eth logo"></img> */}
                     </div>
                     <div id="wallet">
-                      
+
                     </div>
                 </div>
 
@@ -45,33 +49,19 @@ export default function NewDemo() {
             <div className="outer">
                 <div className="row">
                     <h2 className="rowTitle">Popular Content</h2>
-                    <div className="item" id="media" onClick={(e) => openMedia(e)}>
-
-                    </div>
-                    <div className="item" id="media2" onClick={(e) => openMedia(e)}>
-
-                    </div>
-                    <div className="item" id="media3" onClick={(e) => openMedia(e)}>
-
-                    </div>
-                    <div className="item" id="media4" onClick={(e) => openMedia(e)}>
-
-                    </div>
-                    <div className="item" id="media5" onClick={(e) => openMedia(e)}>
-
-                    </div>
-                    <div className="item">
-
-                    </div>
-                    <div className="item">
-
-                    </div>
-                    <div className="item">
-
-                    </div>
-                    <div className="item">
-
-                    </div>
+                    {posts.map(post => {
+                        if (!post.deleted) {
+                            return <Media
+                                className="item"
+                                key={post._id}
+                                id={post._id}
+                                imgUrl={post.imgSrc}
+                                price={post.price}
+                                privileged={post.privileged}
+                                username={post.user}
+                            />
+                        }
+                    })}
                 </div>
                 <div className="row">
                     <h2 className="rowTitle">B00By736's Videos</h2>
