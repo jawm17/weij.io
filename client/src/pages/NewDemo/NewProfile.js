5e08bb48659c0feb20b35f77e305707cfcbfac1a
Michael Sanderson <jawm17@gmail.com>
2020-10-30 00:25:31 -0700

    New profile style

--- client/src/pages/NewDemo/NewProfile.js
+++ client/src/pages/NewDemo/NewProfile.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState, useContext } from 'react';
-
+import "./profileStyle.css";
 
 export default function NewProfile() {
-    
+
+
+
+    return (
+        <div>
+            hellllllowoowowowoo
+        </div>
+    );
 }
