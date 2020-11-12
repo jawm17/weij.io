364a3b38fd7ea0869d2c01c9d1cd6e64bff0c6b1
Michael Sanderson <jawm17@gmail.com>
2020-11-11 16:34:52 -0800

    CoOl CoLoRs

--- client/src/components2/headerAccessed.js
+++ client/src/components2/headerAccessed.js
@@ -4,20 +4,40 @@ import "./headerAccessedStyle.css";
 export default function HeaderAccessed() {
 
 
+    function selectSection(selection) {
+        if(selection === "music") {
+            document.getElementById("mediaLogo").style.color = "gray";
+            document.getElementById("gamesLogo").style.color = "gray";
+            document.getElementById("musicLogo").style.color = "#01CBFF";
+        } 
+        else if (selection === "games"){
+            document.getElementById("mediaLogo").style.color = "gray";
+            document.getElementById("musicLogo").style.color = "gray";
+            document.getElementById("gamesLogo").style.color = "#5DE900";
+        } 
+        else if (selection === "media") {
+            document.getElementById("gamesLogo").style.color = "gray";
+            document.getElementById("musicLogo").style.color = "gray";
+            document.getElementById("mediaLogo").style.color = "#8A62E2";
+        }
+
+    }
+
+
     return (
         <div>
             <div id="header">
                     <div className="logoTextBlock">
-                        <div className="mainLogoA selectable">
+                        <div className="mainLogoA selectable" onClick={() => window.location.href = "/landing"}>
                             ethereal
                         </div>
-                        <div className="mediaLogo selectable">
+                        <div id="mediaLogo" className="selectable" onClick={() => selectSection("media")}>
                             media
                         </div>
-                        <div className="mediaLogo selectable">
+                        <div id="musicLogo" className="selectable" onClick={() => selectSection("music")}>
                             music
                         </div>
-                        <div className="mediaLogo selectable">
+                        <div id="gamesLogo" className="selectable" onClick={() => selectSection("games")}>
                             games
                         </div>
                     </div>
