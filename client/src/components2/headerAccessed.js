0bb3ae65aa770dc1ff4eede4dee5cab4cb83b72c
Michael Sanderson <jawm17@gmail.com>
2020-11-12 08:44:42 -0800

    Notification icons

--- client/src/components2/headerAccessed.js
+++ client/src/components2/headerAccessed.js
@@ -52,6 +52,17 @@ export default function HeaderAccessed() {
                         {/* <img id="ethIcon" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkhipus.io%2Fimages%2Fether-iso.png&f=1&nofb=1"></img>
                         2.01 */}
                         Wallet
+                        <div className="notif">
+                            19
+                        </div>
+                    </div>
+                    <div id="messagesA" className="fixedButtonsA" onClick={() => window.location.href = "/wallet"}>
+                        {/* <img id="ethIcon" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkhipus.io%2Fimages%2Fether-iso.png&f=1&nofb=1"></img>
+                        2.01 */}
+                        Messages
+                        <div className="notif">
+                            2
+                        </div>
                     </div>
                     <div id="profileA" onClick={() => window.location.href = "/profile"}>
                         <img src='https://avatars3.githubusercontent.com/u/56066513?s=460&u=2724432d8929c333aea5ea6751128b6db55c747e&v=4' id="profileIcon"></img>
