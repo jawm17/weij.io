ecd1ff6e9a49841b6a523a85e58ab29169064992
Michael Sanderson <jawm17@gmail.com>
2021-01-05 17:00:38 -0600

    imported post modal

--- client/src/pages/NewPages/Profile.js
+++ client/src/pages/NewPages/Profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext} from "react";
 import UserService from '../../services/UserService';
 import { AuthContext } from '../../context/AuthContext';
 import HeaderAccessed from '../../components2/headerAccessed';
@@ -9,7 +9,10 @@ import "./profileStyle.css";
 
 export default function Profile() {
 
+    const authContext = useContext(AuthContext);
     const [profileImg, setProfileImg] = useState("https://avatars3.githubusercontent.com/u/56066513?s=460&u=2724432d8929c333aea5ea6751128b6db55c747e&v=4");
+    const [creatingPost, setCreatingPost] = useState(false);
+    const [username, setUsername] = useState("");
     // const [top, setTop] = useState(90);
     // const [profilePicTop, setProfilePicTop] = useState(60);
     // const [picWidth, setPicWidth] = useState(120);
@@ -87,33 +90,85 @@ export default function Profile() {
     //     });
     // }
 
+    function requestUserData() {
+        UserService.getUserInfo().then(data => {
+            if (!data.message) {
+                setUsername(data.username);
+                setProfileImg(data.profileImgSrc);
+            }
+            else if (data.message.msgBody === "Unauthorized") {
+                authContext.setUser({ username: "" });
+                authContext.setIsAuthenticated(false);
+                window.alert("not logged in");
+            }
+        });
+    }
+
+
+    function createAPost() {
+        setCreatingPost(true);
+    }
+
+    function doneCreating() {
+        setCreatingPost(false);
+    }
+
+
     return (
         <div>
             <HeaderAccessed secured="t" />
+            {creatingPost ?  <PostModal username={username} profileSrc={profileImg} done={() => doneCreating()}/> : null}
             <div id="profileBg">
                 <div id="infoArea">
-                   <div id="infoAreaFlex">
+                    <div id="infoAreaFlex">
                         <div id="infoAreaContainer">
                             <img id="profilePicture" src={profileImg} alt="profile avatar"></img>
+                            <div id="usernameDisplay">
+                                Jawm42
+                            </div>
+
+                            <div id="infoFlex">
+                                <div className="num">
+                                    350
+                                <div className='label'>
+                                        videos
+                                </div>
+                                </div>
+                                <div className="num">
+                                    2,000,012
+                                <div className='label'>
+                                        subscribers
+                                </div>
+                                </div>
+                                <div className="num">
+                                    4,023
+                                <div className='label'>
+                                        Eth Earned
+                                </div>
+                                </div>
+                            </div>
+                            <div id="postButton" onClick={() => createAPost()}>
+                                +
+                            </div>
                         </div>
-                   </div>
+                    </div>
                 </div>
                 <div id="fixedView">
                     <div id="flexContainer">
-                 
-                            {media.map(video => {
-                                if (!video.deleted) {
-                                    return <MediaPreview
-                                        key={video.thumb}
-                                        id={video.thumb}
-                                        imgUrl={video.sources}
-                                        price={video.price}
-                                        privileged={video.privileged}
-                                        username={video.user}
-                                    />
-                                }
-                            })}
-                      
+
+                        {media.map(video => {
+                            if (!video.deleted) {
+                                return <MediaPreview
+                                    key={video.thumb}
+                                    id={video.thumb}
+                                    imgUrl={video.sources}
+                                    price={video.price}
+                                    privileged={video.privileged}
+                                    username={video.user}
+                                />
+                            }
+                        })}
+
                     </div>
                 </div>
             </div>
