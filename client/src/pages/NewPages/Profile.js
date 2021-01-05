2753c733d2cc291e0a1204578683b731da2ca8b5
Michael Sanderson <jawm17@gmail.com>
2021-01-05 16:20:22 -0600

    Profile info area unstuck

--- client/src/pages/NewPages/Profile.js
+++ client/src/pages/NewPages/Profile.js
@@ -10,65 +10,65 @@ import "./profileStyle.css";
 export default function Profile() {
 
     const [profileImg, setProfileImg] = useState("https://avatars3.githubusercontent.com/u/56066513?s=460&u=2724432d8929c333aea5ea6751128b6db55c747e&v=4");
-    const [top, setTop] = useState(90);
-    const [profilePicTop, setProfilePicTop] = useState(60);
-    const [picWidth, setPicWidth] = useState(120);
-    const [nameTop, setNameTop] = useState(195);
-    const [picLeft, setPicLeft] = useState(80);
-    const [fixedTop, setFixedTop] = useState(300);
-    const [bgTop, setBgTop] = useState(0);
+    // const [top, setTop] = useState(90);
+    // const [profilePicTop, setProfilePicTop] = useState(60);
+    // const [picWidth, setPicWidth] = useState(120);
+    // const [nameTop, setNameTop] = useState(195);
+    // const [picLeft, setPicLeft] = useState(80);
+    // const [fixedTop, setFixedTop] = useState(300);
+    // const [bgTop, setBgTop] = useState(0);
     let scrollArea;
 
-    const style = {
-        infoArea: {
-            top: top,
-            backgroundPositionY: bgTop
-        },
-        profilePic: {
-            marginTop: profilePicTop,
-            marginLeft: picLeft,
-            width: picWidth
-        },
-        username: {
-            top: nameTop,
-            left: 240
-        },
-        fixedView: {
-            marginTop: fixedTop
-        },
-        infoFlex: {
-            top: nameTop - 10
-        }
-    }
+    // const style = {
+    //     infoArea: {
+    //         top: top,
+    //         backgroundPositionY: bgTop
+    //     },
+    //     profilePic: {
+    //         marginTop: profilePicTop,
+    //         marginLeft: picLeft,
+    //         width: picWidth
+    //     },
+    //     username: {
+    //         top: nameTop,
+    //         left: 240
+    //     },
+    //     fixedView: {
+    //         marginTop: fixedTop
+    //     },
+    //     infoFlex: {
+    //         top: nameTop - 10
+    //     }
+    // }
 
-    useEffect(() => {
-        scrollArea = document.getElementById("profileBg");
-        scrollArea.addEventListener("scroll", () => {
-            checkScroll();
-        });
+    // useEffect(() => {
+    //     scrollArea = document.getElementById("profileBg");
+    //     scrollArea.addEventListener("scroll", () => {
+    //         checkScroll();
+    //     });
 
-    });
+    // });
 
-    function checkScroll() {
-        if (scrollArea.scrollTop < 10) {
-            // initial
-            setTop(90);
-            setProfilePicTop(60);
-            setPicWidth(120);
-            setNameTop(195);
-            setPicLeft(80);
-            setFixedTop(300);
-            setBgTop(0);
-        } else {
-            setTop(12);
-            setProfilePicTop(110);
-            setPicWidth(90);
-            setNameTop(156);
-            setFixedTop(220);
-            setPicLeft(100);
-            setBgTop(-180);
-        }
-    }
+    // function checkScroll() {
+    //     if (scrollArea.scrollTop < 10) {
+    //         // initial
+    //         setTop(90);
+    //         setProfilePicTop(60);
+    //         setPicWidth(120);
+    //         setNameTop(195);
+    //         setPicLeft(80);
+    //         setFixedTop(300);
+    //         setBgTop(0);
+    //     } else {
+    //         setTop(12);
+    //         setProfilePicTop(110);
+    //         setPicWidth(90);
+    //         setNameTop(156);
+    //         setFixedTop(220);
+    //         setPicLeft(100);
+    //         setBgTop(-180);
+    //     }
+    // }
 
     // function getInfo() {
     //     UserService.getUserInfo().then(data => {
@@ -91,33 +91,10 @@ export default function Profile() {
         <div>
             <HeaderAccessed secured="t" />
             <div id="profileBg">
-                <div id="infoArea" className="sharpWidth" style={style.infoArea}>
-                    <img id="profilePicture" className="smoothAll" src={profileImg} style={style.profilePic} alt="profile picture"></img>
-                    <div id="usernameDisplay" className="smoothAll" style={style.username}>
-                        Jawm42
-                        </div>
-                    <div id="infoFlex" className="smoothAll" style={style.infoFlex}>
-                        <div className="num">
-                            350
-                                <div className='label'>
-                                videos
-                                </div>
-                        </div>
-                        <div className="num">
-                            2,000,012
-                                <div className='label'>
-                                subscribers
-                                </div>
-                        </div>
-                        <div className="num">
-                            4,023
-                                <div className='label'>
-                                Eth Earned
-                                </div>
-                        </div>
-                    </div>
+                <div id="infoArea" className="sharpWidth">
+                   
                 </div>
-                <div id="fixedView" className="smoothTop" style={style.fixedView}>
+                <div id="fixedView" className="smoothTop">
                     <div id="flexContainer">
                  
                             {media.map(video => {
