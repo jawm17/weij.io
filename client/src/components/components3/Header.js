bb3de0306718e4354a06f5cc7b7b70f00aed708c
Michael Sanderson <jawm17@gmail.com>
2021-02-04 16:05:46 -0600

    header icons

--- client/src/components/components3/Header.js
+++ client/src/components/components3/Header.js
@@ -29,13 +29,13 @@ export default function Header(props) {
                 </div>
                 <div id="headerRight">
                     <div className="relativeOuter">
-                        <img src="https://firebasestorage.googleapis.com/v0/b/weij-c2efd.appspot.com/o/home2.png?alt=media&token=dd271323-9bc6-482e-b228-4ccb2b8c554c" id="homeIcon" className="headerIcon" onClick={() => history.push("/")}></img>
+                        <img src={home ? "https://firebasestorage.googleapis.com/v0/b/weij-c2efd.appspot.com/o/home2.png?alt=media&token=dd271323-9bc6-482e-b228-4ccb2b8c554c" : "https://firebasestorage.googleapis.com/v0/b/weij-c2efd.appspot.com/o/homeEmpty.png?alt=media&token=e89e81fd-4f26-46ce-97b5-91a5295fbbc7"} id="homeIcon" className="headerIcon" onClick={() => history.push("/")}></img>
                         <div className="blue" id="blueHome" style={home ? { "display": "initial" } : { "display": "none" }}>
 
                         </div>
                     </div>
                     <div className="relativeOuter">
-                        <img src="https://firebasestorage.googleapis.com/v0/b/weij-c2efd.appspot.com/o/in2.png?alt=media&token=815ade2b-76cf-4ee1-b420-435c65fb850b" className="headerIcon" onClick={() => history.push("/send")}></img>
+                        <img src={send ? "https://firebasestorage.googleapis.com/v0/b/weij-c2efd.appspot.com/o/sendFull.png?alt=media&token=501f807c-3ead-4539-bb30-42ec2dc85b06" : "https://firebasestorage.googleapis.com/v0/b/weij-c2efd.appspot.com/o/in2.png?alt=media&token=815ade2b-76cf-4ee1-b420-435c65fb850b"} className="headerIcon" onClick={() => history.push("/send")}></img>
                         <div id="notif">
 
                         </div>
@@ -44,7 +44,7 @@ export default function Header(props) {
                         </div>
                     </div>
                     <div className="relativeOuter">
-                        <img src="https://firebasestorage.googleapis.com/v0/b/weij-c2efd.appspot.com/o/wallet2.png?alt=media&token=bebf1983-0453-4740-b297-c23b94ad7a6c" className="headerIcon" onClick={() => history.push("/wallet")}></img>
+                        <img src={wallet ? "https://firebasestorage.googleapis.com/v0/b/weij-c2efd.appspot.com/o/walletFull.png?alt=media&token=0d34285b-6f0d-41d3-8d32-73fce260fd6c" : "https://firebasestorage.googleapis.com/v0/b/weij-c2efd.appspot.com/o/wallet2.png?alt=media&token=bebf1983-0453-4740-b297-c23b94ad7a6c"} className="headerIcon" onClick={() => history.push("/wallet")}></img>
                         <div id="notif">
 
                         </div>
