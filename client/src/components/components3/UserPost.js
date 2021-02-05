cabdf666feed36ad8fad8285c2bd8f3de4317087
Michael Sanderson <jawm17@gmail.com>
2021-02-04 20:07:06 -0600

    basic grid

---
+++ client/src/components/components3/UserPost.js
@@ -0,0 +1,11 @@
+import React from "react";
+import  "./styles/userPostStyle.css";
+
+export default function UserPost() {
+
+    return (
+        <div className="userPost">
+            
+        </div>
+    );
+}
--- client/src/pages/NewerPages/UserPage.js
+++ client/src/pages/NewerPages/UserPage.js
@@ -1,43 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from '../../components/components3/Header';
+import UserPost from "../../components/components3/UserPost";
+import videos from "../../videos.json";
 import "./styles/userPageStyle.css";
 
 export default function UserPage() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        setPosts(videos);
+    });
 
     return (
         <div>
-            <Header page="profile"/>
+            <Header page="profile" />
             <div id="outerProfileBody">
                 <div id="profileBody">
                     <div id="profileInfo">
                         <div>
                             <img id="profileImage" src="https://lh3.googleusercontent.com/ogw/ADGmqu9lR4YTUnjkgrgeuoE-JBqu8RHR4wzA8Xum5zfqWw=s64-c-mo"></img>
                         </div>
-                        <div id="profileUsername">
+                        <div id="profileInfoArea">
                             Jawm42
                             <div id="profileNumbers">
-                                <div>
+                                <div className="number">
                                     200 followers
                                 </div>
-                                <div>
+                                <div className="number">
+                                    3.24 eth earned
+                                </div>
+                                <div className="number">
                                     20 posts
                                 </div>
                             </div>
                             <div id="profileButtons">
-                                <button>
+                                <div className="profileButton">
+                                    create post
+                                </div>
+                                <div className="profileButton">
+                                    following
+                                </div>
+                                <div className="profileButton">
                                     settings
-                                </button>
-                                <button>
-                                        create post
-                                </button>
-                                <button>
-                                    share
-                                </button>
+                                </div>
                             </div>
                         </div>
+
                     </div>
                     <div id="line">
 
+                    </div>
+
+                    <div id="grid">
+                        {posts.map(post => {
+                            if (!post.deleted) {
+                                return <UserPost
+                                    // className="mediaCard"
+                                    // getBalance={props.getBalance}
+                                    key={post.description}
+                                    descript={post.description}
+                                    color={post.color}
+                                // imgUrl={post.imgSrc}
+                                // price={post.price}
+                                // privileged={post.privileged}
+                                // username={post.user}
+                                // ethPrice={props.ethPrice}
+                                />
+                            }
+                        })}
                     </div>
                 </div>
             </div>
