bb1a7fc36ff6d3c07b1c4d21c47f5c63d4506b16
Michael Sanderson <jawm17@gmail.com>
2021-02-17 18:15:38 -0600

    body styling

--- client/src/pages/NewerPages/Body.js
+++ client/src/pages/NewerPages/Body.js
@@ -37,19 +37,22 @@ export default function Body(props) {
                     <div id="catBox" onClick={() => history.push("/profile")}>
                         <img src="https://avatars3.githubusercontent.com/u/56066513?s=460&u=2724432d8929c333aea5ea6751128b6db55c747e&v=4" id="catPhoto"></img>
                         Jawm42
-                        <div id="signOut">
+                        <div className="label">
                             account
                         </div>
                     </div>
                     <div id="catBox" onClick={() => history.push("/wallet")}>
                         <img src="https://firebasestorage.googleapis.com/v0/b/weij-c2efd.appspot.com/o/eth%20(1).png?alt=media&token=8675ca36-6b00-44e2-b609-245a7fe89998" id="catPhoto"></img>
                         1.03044 ETH
+                        <div className="label">
+                            wallet
+                        </div>
                     </div>
                     <div id="suggestions">
                         Suggestions for you
                         <div className="suggest">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjRstf5ncFDVYX3duv7BF_-AwpNnqQb3pYSQ&usqp=CAU" className="suggestPhoto"></img>
-                            ChampagnePapi
+                            joesl90
                         </div>
                         <div className="suggest">
                             <img src="https://www.usmagazine.com/wp-content/uploads/2020/09/Katy-Perry-Says-Motherhood-Is-A-Full-Time-Job-After-Welcoming-Daughter-Daisy-Promo.jpg?w=700&quality=86&strip=all" className="suggestPhoto"></img>
@@ -57,7 +60,18 @@ export default function Body(props) {
                         </div>
                         <div className="suggest">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjRstf5ncFDVYX3duv7BF_-AwpNnqQb3pYSQ&usqp=CAU" className="suggestPhoto"></img>
-                        joesl90
+                            ChampagnePapi  
+                        </div>
+                    </div>
+                    <div id="links">
+                        <div className="link">
+                            terms
+                        </div>
+                        <div className="link">
+                            copyright
+                        </div>
+                        <div className="link">
+                            about
                         </div>
                     </div>
                 </div>
