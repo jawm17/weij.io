ad6a21ceb527b8577b4e05fa9df56699a9566c3b
Michael Sanderson <jawm17@gmail.com>
2021-03-08 23:54:23 -0600

    carousel

--- client/src/pages/SectionsPage/sectionsPage.js
+++ client/src/pages/SectionsPage/sectionsPage.js
@@ -3,6 +3,20 @@ import "./sections.css";
 import MyCarousel from './Carousel';
 
 export default function Sections() {
+
+
+  function scrollRight() {
+    let row = document.getElementById("carousel");
+    row.scrollLeft += 310;
+  }
+
+  function scrollLeft() {
+    let row = document.getElementById("carousel");
+    row.scrollLeft += -310;
+  }
+
+
+
   return (
     <div>
       <div className="Content-container">
@@ -51,7 +65,7 @@ export default function Sections() {
         </div>
         <div className="video-container">
           <div className="video-content">
-          <div className="words-container">
+            <div className="words-container">
               <div className="video-header-container">
                 <h1>Video</h1>
               </div>
@@ -93,7 +107,7 @@ export default function Sections() {
         </div>
         <div className="game-container">
           <div className="game-content">
-          <div className="words-container">
+            <div className="words-container">
               <div className="game-header-container">
                 <h1>Games</h1>
               </div>
@@ -104,33 +118,38 @@ export default function Sections() {
                 </p>
               </div>
             </div>
-            <div className="artist-container">
-              <div className="artist">
-                <div className="artist1">
-                  <h1>asap</h1>
-                  <div className="artist-img">
-                    <img src={require("./asap.jpeg")} />
-                  </div>
+            <div id="carouselContainer">
+              <div className="carousel" id="carousel">
+                <div className="carouselItem">
+
                 </div>
-                <div className="artist2">
-                  <h1>frank</h1>
-                  <div className="artist-img">
-                    <img src={require("./frank.jpeg")} />{" "}
-                  </div>
+                <div className="carouselItem">
+
                 </div>
-                <div className="artist3">
-                  <h1>travis</h1>
-                  <div className="artist-img">
-                    <img src={require("./travis.jpeg")} />{" "}
-                  </div>
+                <div className="carouselItem">
+
                 </div>
-                <div className="artist4">
-                  <h1>tyler</h1>
-                  <div className="artist-img">
-                    <img src={require("./tyler.jpeg")} />{" "}
-                  </div>
+                <div className="carouselItem">
+
+                </div>
+                <div className="carouselItem">
+
+                </div>
+                <div className="carouselItem">
+
+                </div>
+                <div className="carouselItem">
+
+                </div>
+                <div className="carouselItem">
+
+                </div>
+                <div className="carouselItem">
+
                 </div>
               </div>
+              <img onClick={() => scrollLeft()} src="https://freeiconshop.com/wp-content/uploads/edd/arrow-flat.png" id="backwardArrow" className="arrow"></img>
+              <img onClick={() => scrollRight()} src="https://freeiconshop.com/wp-content/uploads/edd/arrow-flat.png" id="forwardArrow" className="arrow"></img>
             </div>
           </div>
         </div>
